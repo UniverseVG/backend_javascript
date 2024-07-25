3f7363a8d6e2cba8b5cee98a0369ce6a9fcc048a
UniverseVG <gmvarun2001@gmail.com>
2024-07-25 15:54:49 +0530

    add user and video model

--- video_player_backend/src/index.js
+++ video_player_backend/src/index.js
@@ -2,12 +2,23 @@ import mongoose from "mongoose";
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App Listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Error: ", error);
+  });
 
 // one type of adding mongo connection, app listening
 
