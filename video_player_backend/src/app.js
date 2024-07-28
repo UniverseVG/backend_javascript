65d73648c21b39edc075762b50981398683190ec
UniverseVG <gmvarun2001@gmail.com>
2024-07-28 20:35:48 +0530

    add controllers and routes skeleton

--- video_player_backend/src/app.js
+++ video_player_backend/src/app.js
@@ -24,10 +24,26 @@ app.use(cookieParser());
 //routes import
 
 import userRouter from "./routes/user.route.js";
+import healthcheckRouter from "./routes/healthcheck.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import videoRouter from "./routes/video.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import playlistRouter from "./routes/playlist.route.js";
+import dashboardRouter from "./routes/dashboard.route.js";
 
 const prefixURLVersion = "/api/v1";
 
 //routes declaration
+app.use(`${prefixURLVersion}/healthcheck`, healthcheckRouter);
 app.use(`${prefixURLVersion}/users`, userRouter);
+app.use(`${prefixURLVersion}/tweets`, tweetRouter);
+app.use(`${prefixURLVersion}/subscriptions`, subscriptionRouter);
+app.use(`${prefixURLVersion}/videos`, videoRouter);
+app.use(`${prefixURLVersion}/comments`, commentRouter);
+app.use(`${prefixURLVersion}/likes`, likeRouter);
+app.use(`${prefixURLVersion}/playlist`, playlistRouter);
+app.use(`${prefixURLVersion}/dashboard`, dashboardRouter);
 
 export default app;
